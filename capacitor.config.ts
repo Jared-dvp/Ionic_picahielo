7a237abc49dfeda88883559295a52e9a10fdef61
Jared-dvp <jared.trujilloespinosa@unicolombo.edu.co>
2024-09-24 10:27:06 -0500

    create proyect, create module shared ande create page register

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sec_par_unicol',
+  webDir: 'www'
+};
+
+export default config;
